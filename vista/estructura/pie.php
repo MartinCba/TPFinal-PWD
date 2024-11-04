3ccb9448b65dc96abea079a6a6b5df3e80bbba0e
Martin Hernandez <martinalejandrohernandez1@gmail.com>
2024-11-04 11:18:54 -0300

    estructura/pie

---
+++ vista/estructura/pie.php
@@ -0,0 +1,35 @@
+</div>
+</div>
+</div>
+</section> <!-- Contenido Principal FIN -->
+
+<footer class="bg-dark bg-gradient text-center text-lg-start fixed-bottom"> <!-- Pie INICIO -->
+    <div class="text-center text-white my-1">
+        <ul class="navbar-nav d-flex flex-row justify-content-center">
+            <li class="nav-item mx-1">
+                <a href="../paginas/quienesSomos.php" class="nav-link text-white">Sobre nosotros</a>
+            </li>
+            <li class="nav-item mx-1">
+                <a href="../paginas/quienesSomos.php" class="nav-link text-white btn disabled">|</a>
+            </li>
+            <li class="nav-item mx-1">
+                <a href="../paginas/acercaDe.php" class="nav-link text-white">Acerca Del proyecto</a>
+            </li>
+            <li class="nav-item mx-1">
+                <a href="../paginas/quienesSomos.php" class="nav-link text-white btn disabled">|</a>
+            </li>
+            <li class="nav-item mx-1">
+                <a href="../paginas/contactenos.php" class="nav-link text-white">Contacto</a>
+            </li>
+        </ul>
+    </div>
+    <div class="text-center text-white my-1">
+        Programación Web Dinámica
+    </div>
+</footer> <!-- Pie FINAL -->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
