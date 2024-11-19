bc19e705de0d9a86ef9da9a9b14322a301a125b9
Martin Hernandez <martinalejandrohernandez1@gmail.com>
2024-11-19 18:56:30 -0300

    ajuste en configuracion.php

--- configuracion.php
+++ configuracion.php
@@ -11,6 +11,5 @@ $ROOT = $_SERVER["DOCUMENT_ROOT"] . "/TPFinal-PWD/";
 
 include_once($ROOT . "util/funciones.php");
 
-// Almacena la ruta del directorio raíz del proyecto en una variable de sesión.
-// Esto permite que la ruta raíz esté disponible en diferentes páginas y scripts durante la sesión del usuario.
-$_SESSION["ROOT"] = $ROOT;
+// Almacena la ruta del directorio raíz del proyecto en una variable global.
+$GLOBALS["ROOT"] = $ROOT;
